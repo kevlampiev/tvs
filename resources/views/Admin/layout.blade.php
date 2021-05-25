d98c0b5b5d87f38fbdca18a4816c18e13ba15928
Константин Евлампиев <kevlamiev@gmail.com>
2021-05-25 22:51:18 +0700

    addev views

--- resources/views/Admin/layout.blade.php
+++ resources/views/Admin/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" type="text/css">
     <script src="{{ mix('js/app.js') }}"></script>
 </head>
@@ -47,17 +47,11 @@
     </div>
 </nav>
 
-
-
-<div class="jumbotron">
-    <h1 class="display-4">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </p>
+<div class="container">
+    @yield('content')
 </div>
+
 
 </body>
 </html>
+

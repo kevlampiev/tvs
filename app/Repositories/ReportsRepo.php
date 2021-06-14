ca5a43a9aa2b9e9aaf81657a6053cf128482557e
Konstantin Evlampiev <kevlampiev@gmail.com>
2021-06-14 09:09:01 +0300

    started first report

---
+++ app/Repositories/ReportsRepo.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Models\Agreement;
+use App\Models\Vehicle;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class ReportsRepo
+{
+
+    static public function getSettlementStatusData(Request $request)
+    {
+
+    }
+
+}

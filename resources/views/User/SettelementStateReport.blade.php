beab000547431da1a4e107a2ee1e1220df5a63f6
kevlampiev <kevlampiev@gmail.com>
2021-06-16 13:39:35 +0700

    added formats to settlements report

--- resources/views/User/SettelementStateReport.blade.php
+++ resources/views/User/SettelementStateReport.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.app')
 
+
 @section('content')
 {{--    @dd($data)--}}
-    <div class="container">
+
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="com-md-12">
+                <h3>Задолженость по финансовым договорам</h3>
+                <form mephod="GET">
+
+                </form>
+            </div>
+        </div>
         <div class="row justify-content-center">
             <div class="col-md-12">
+
                 @foreach($data as $key=>$company)
                     <h4 class="mt-lg-4"> {{$key}}</h4>
-                    <table class="table table-striped">
+                    <table class="table">
                         <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -24,23 +35,34 @@
                         <tbody>
                         @foreach($company as $index=>$el)
                         <tr>
-                            <th scope="row">{{$index}}</th>
+                            <th scope="row">{{$index+1}}</th>
                             <td>{{$el->counterparty_name}}</td>
                             <td>{{$el->agreementType->name}}</td>
                             <td>{{$el->agr_number}} от {{$el->date_open}}</td>
                             <td>
                                 <ul>
-                                @foreach($el->vehicles as $vehicle)
-                                    <li>{{$vehicle->vehicleType->name}}/{{$vehicle->name}} /{{$vehicle->bort_number}}</li>
-                                @endforeach
+                                @forelse($el->vehicles as $vehicle)
+                                    <li>{{$vehicle->vehicleType->name}}  {{$vehicle->name}}  {{$vehicle->bort_number}}</li>
+                                @empty
+                                    -----
+                                @endforelse
                                 </ul>
                             </td>
-                            <td>{{$el->principal_amount}} от {{$el->currency}}</td>
-                            <td>{{$el->total_payments}}</td>
-                            <td>{{$el->payed}}</td>
-                            <td>{{$el->must_be_payed_by_date - $el->payed}}  </td>
+                            <td class="text-right">{{number_format($el->principal_amount,2)}} {{$el->currency}}</td>
+                            <td class="text-right">{{number_format($el->total_payments,2)}}</td>
+                            <td class="text-right">{{number_format($el->payed,2)}}</td>
+                            <td class="text-right">{{number_format($el->must_be_payed_by_date - $el->payed,2)}}  </td>
                         </tr>
                         @endforeach
+{{--                        @dd($company)--}}
+                        <tr>
+                            <th colspan="4">Всего </th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
                         </tbody>
                     </table>
                 @endforeach

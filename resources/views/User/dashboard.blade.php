01bdd8dd0212fe4012ae14892e93d3b6b460abba
Константин Евлампиев <kevlamiev@gmail.com>
2021-06-24 21:39:44 +0700

    added draft of dashboard

---
+++ resources/views/User/dashboard.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+
+@section('content')
+    {{--    @dd($data)--}}
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="shadow p-3 mb-5 bg-white rounded">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aperiam doloribus et quos! Ab assumenda consequatur excepturi illo libero nihil, nisi provident quas quod reprehenderit sapiente similique soluta ullam.
+                </div>
+                <div class="shadow p-3 mb-5 bg-white rounded">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet autem, consequuntur deserunt dignissimos ea id ipsam laboriosam laborum molestiae mollitia nemo nobis nostrum officiis perspiciatis, porro quisquam totam voluptatum. Blanditiis.
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="shadow p-3 mb-5 bg-white rounded">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur, distinctio expedita modi placeat temporibus velit. Incidunt itaque obcaecati rerum!
+                </div>
+                <div class="shadow p-3 mb-5 bg-white rounded">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, quam similique. Aperiam enim facere libero.
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection

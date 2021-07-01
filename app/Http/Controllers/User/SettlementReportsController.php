6b07cc7455fd993ff9125fca4ba17c26f13229f2
Константин Евлампиев <kevlamiev@gmail.com>
2021-07-01 20:28:59 +0700

    renaming

--- app/Http/Controllers/User/SettlementReportsController.php
+++ app/Http/Controllers/User/SettlementReportsController.php
@@ -5,26 +5,26 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Agreement;
 use App\Repositories\DashboardsRepo;
-use App\Repositories\ReportsRepo;
+use App\Repositories\SettlementsRepo;
 use Illuminate\Http\Request;
 
 class SettlementReportsController extends Controller
 {
     public function showBigSettlementReport(Request $request)
     {
-        return view('User.SettelementStateReport',
-            ReportsRepo::getBigSettlementReportData($request));
+        return view('User.SettlementStateReport',
+            SettlementsRepo::getBigSettlementReportData($request));
     }
 
     public function showBigSettlement2Report(Request $request)
     {
-        return view('User.SettelementState2Report',
-            ReportsRepo::getBigSettlementReport2Data($request));
+        return view('User.SettlementState2Report',
+            SettlementsRepo::getBigSettlementReport2Data($request));
     }
 
     public function showAgrSettlementReport(Request $request, Agreement $agreement)
     {
-        return view('User.SettelementAgreementReport',
-            ReportsRepo::getAgreemantSettlementReportData($request, $agreement));
+        return view('User.SettlementAgreementReport',
+            SettlementsRepo::getAgreemantSettlementReportData($request, $agreement));
     }
 }

1a53c009443ca4e0831b952fe630f65ff4368bf5
Константин Евлампиев <kevlamiev@gmail.com>
2022-01-19 00:07:35 +0700

    removed some agreement data 3 from agreement payments export

--- app/Exports/AgreementPaymentsExport.php
+++ app/Exports/AgreementPaymentsExport.php
@@ -15,7 +15,6 @@ class AgreementPaymentsExport implements FromView
     public function view(): View
     {
         $payments = AgreementPayment::all();
-        dd($payments);
 //        $payments = DB::select('select ');
         return view('exports.agreement-payments', ['payments' => $payments]);
     }

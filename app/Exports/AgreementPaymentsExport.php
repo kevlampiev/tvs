9fbc6edc6cc6d194e3ea29c810e1b086ac9a7c80
Константин Евлампиев <kevlamiev@gmail.com>
2022-01-19 00:05:14 +0700

    removed some agreement data from agreement payments export

--- app/Exports/AgreementPaymentsExport.php
+++ app/Exports/AgreementPaymentsExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Models\AgreementPayment;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromView;
 
 class AgreementPaymentsExport implements FromView
@@ -13,7 +14,8 @@ class AgreementPaymentsExport implements FromView
      */
     public function view(): View
     {
-        $payments = AgreementPayment::all();
+//        $payments = AgreementPayment::all();
+        $payments = DB::select('select ');
         return view('exports.agreement-payments', ['payments' => $payments]);
     }
 }

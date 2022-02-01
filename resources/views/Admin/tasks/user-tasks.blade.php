e3a32587ac217ad969afb9ae439914b5a242a616
Константин Евлампиев <kevlamiev@gmail.com>
2022-02-01 21:45:00 +0700

    added filter for user tasks

--- resources/views/Admin/tasks/user-tasks.blade.php
+++ resources/views/Admin/tasks/user-tasks.blade.php
@@ -7,7 +7,21 @@
 @section('content')
 
     <div class="row">
-        <h2>Мои задачи</h2>
+        <div class="col-md-6">
+            <h2>Мои задачи</h2>
+        </div>
+        <div class="col-md-6">
+            <form class="form-inline my-2 my-lg-0" method="GET">
+                <input class="form-control mr-sm-2" type="search" placeholder="Поиск в моих задачах" aria-label="Search" name="searchStr"
+                       value="{{isset($filter)?$filter:''}}">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Поиск</button>
+            </form>
+        </div>
+
+    </div>
+
+    <div class="row">
+
         <div class="col-md-12">
             <div id="accordion">
                 <div class="card">

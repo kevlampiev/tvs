7115a82891dd59cc6e11ffbec8209bd36418cec6
Konstantin Evlampiev <kevlampiev@gmail.com>
2022-02-01 18:33:52 +0300

    removed limitation (#148)
    Co-authored-by: Константин Евлампиев <kevlamiev@gmail.com>

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -71,7 +71,7 @@
 
                                     @endif
                                 >
-                                    {{\Illuminate\Support\Str::limit($message->description,90,$end='...')}}
+                                    {{$message->description}}
                                 </a>
                                 <span class="small text-secondary"> {{\Carbon\Carbon::parse($message->created_at)->format('d.m.Y')}}
                                     автор: {{$message->user->name}}</span>

3669c4d8524ffefc2a2bfa39c61e9264c5870cf3
kevlampiev <kevlampiev@gmail.com>
2022-02-02 22:01:35 +0700

    created migrations and models for Deposits, Guaranties for individuals, Guaranties for Legal Entities

---
+++ app/Http/Controllers/GuaranteeLegalEntityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GuaranteeLegalEntity;
+use Illuminate\Http\Request;
+
+class GuaranteeLegalEntityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GuaranteeLegalEntity  $guaranteeLegalEntity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GuaranteeLegalEntity $guaranteeLegalEntity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GuaranteeLegalEntity  $guaranteeLegalEntity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GuaranteeLegalEntity $guaranteeLegalEntity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GuaranteeLegalEntity  $guaranteeLegalEntity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GuaranteeLegalEntity $guaranteeLegalEntity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GuaranteeLegalEntity  $guaranteeLegalEntity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GuaranteeLegalEntity $guaranteeLegalEntity)
+    {
+        //
+    }
+}

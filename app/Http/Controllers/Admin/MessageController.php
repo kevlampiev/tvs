98775ed6c6a9ac5076dcfd5ccd33d95a0f94b310
Константин Евлампиев <kevlamiev@gmail.com>
2022-02-17 20:08:07 +0700

    changed controller

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\DataServices\Admin\MessagesDataservice;
+use App\Events\NewCommentToTheTaskReceived;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MessageRequest;
 use App\Models\Message;
@@ -26,6 +27,21 @@ class MessageController extends Controller
     public function store(MessageRequest $request, Message $message): \Illuminate\Http\RedirectResponse
     {
         MessagesDataservice::store($request);
+
+        //TODO Пеоренести этого говнокод в отдельный сервис. Подумать о стркутуре таких сервисов
+        //оповещаем только того юзера, который НЕ ОСТАВЛЯЛ этот комментарий
+//        $task=$message->task;
+//        while(!$task) {
+//            $message = $message->reply_o
+//            $task = $m->task;
+//        }
+//        if (auth()->user()->id != $task->user->id) {
+//            NewCommentToTheTaskReceived::dispatch($task->user, $task);
+//        }
+//        if (auth()->user()->id != $task->performer->id) {
+//            NewCommentToTheTaskReceived::dispatch($task->performer, $task);
+//        }
+        //а еще могут быть подписчики ..... и их тоже надо уведомить
         $route = session('previous_url');
         return redirect()->to($route);
     }

ed9b1791409f5661a0c118ce559bcf5713315eaa
Константин Евлампиев <kevlamiev@gmail.com>
2022-02-20 10:14:15 +0700

    changed user tasks view

--- resources/views/Admin/tasks/user-tasks.blade.php
+++ resources/views/Admin/tasks/user-tasks.blade.php
@@ -7,10 +7,10 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <h2>Мои задачи</h2>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <form class="form-inline my-2 my-lg-0" method="GET">
                 <input class="form-control mr-sm-2" type="search" placeholder="Поиск в моих задачах" aria-label="Search" name="searchStr"
                        value="{{isset($filter)?$filter:''}}">
@@ -22,21 +22,9 @@
 
     <div class="row">
 
-        <div class="col-md-12">
-
-            <div id="accordion">
-                <div class="card">
-                    <div class="card-header" id="assignedToMe">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Задачи, где я - исполнитель <span class="badge badge-secondary">{{count($userAssignments)}}</span>
-                            </button>
-                        </h5>
-                    </div>
-
-                    <div id="collapseOne" class="collapse show" aria-labelledby="assignedToMe" data-parent="#accordion">
-                        <div class="card-body">
-                            <table class="table ">
+        <div class="col-md-6">
+            <h5 class="mt-2">Задачи, поставленные передо мной</h5>
+            <table class="table ">
                                 <thead>
                                 <tr>
                                     <th scope="col">№</th>
@@ -96,82 +84,66 @@
                                     @endforelse
                                 </tbody>
                             </table>
-                        </div>
-                    </div>
-                </div>
+        </div>
 
+        <div class="col-md-6">
+            <h5 class="mt-2">Задачи, поставленные мною другим сотрудникам</h5>
+            <table class="table table-secondary">
+                <thead>
+                <th scope="col">№</th>
+                <th scope="col">Арт</th>
+                    <th scope="col">Формулировка</th>
+                    <th scope="col">Срок исполнения</th>
+                    <th scope="col">Исполнитель</th>
+                    <th scope="col"></th>
+                </thead>
+                <tbody>
+                @forelse($assignedByUser as $task)
+                    <tr class="{{$task->importance=='medium'?'table-light':''}}{{$task->importance=='high'?'table-danger':''}}">
+                        <th>
+                            {{$loop->index}}
+                        </th>
+                        <td class="text-secondary small mr-3">
+                            #{{$task->id}}
+                        </td>
+                        <td>
+                            <a href="{{route('admin.taskCard', ['task' => $task])}}">
+                                {{$task->subject}}
+                            </a>
+                        </td>
+                        <td
+                            class="text-monospace
+                                    @if(($task->due_date<\Carbon\Carbon::now())&&(!$task->terminate_date))
+                                        text-danger
+                                    @elseif((\Carbon\Carbon::parse($task->due_date)->diffInDays(now())<3)&&(!$task->terminate_date))
+                                        text-warning
+                                    @else
+                                        text-secondary small
+                                    @endif
+                                ">
+                            до {{\Carbon\Carbon::parse($task->due_date)->format('d.m.Y')}}
+                        </td>
+                        <td class="text-secondary small mr-3">
+                            {{$task->performer->name}}
+                        </td>
+                        <td>
 
-                <div class="card">
-                    <div class="card-header" id="myAssignments">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Назначенные мной <span class="badge badge-secondary">{{count($assignedByUser)}}</span>
-                            </button>
-                        </h5>
-                    </div>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="myAssignments" data-parent="#accordion">
-                        <div class="card-body">
-                            <table class="table table-secondary">
-                                <thead>
-                                <th scope="col">№</th>
-                                <th scope="col">Арт</th>
-                                    <th scope="col">Формулировка</th>
-                                    <th scope="col">Срок исполнения</th>
-                                    <th scope="col">Исполнитель</th>
-                                    <th scope="col"></th>
-                                </thead>
-                                <tbody>
-                                @forelse($assignedByUser as $task)
-                                    <tr class="{{$task->importance=='medium'?'table-light':''}}{{$task->importance=='high'?'table-danger':''}}">
-                                        <th>
-                                            {{$loop->index}}
-                                        </th>
-                                        <td class="text-secondary small mr-3">
-                                            #{{$task->id}}
-                                        </td>
-                                        <td>
-                                            <a href="{{route('admin.taskCard', ['task' => $task])}}">
-                                                {{$task->subject}}
-                                            </a>
-                                        </td>
-                                        <td
-                                            class="text-monospace
-                                                    @if(($task->due_date<\Carbon\Carbon::now())&&(!$task->terminate_date))
-                                                        text-danger
-                                                    @elseif((\Carbon\Carbon::parse($task->due_date)->diffInDays(now())<3)&&(!$task->terminate_date))
-                                                        text-warning
-                                                    @else
-                                                        text-secondary small
-                                                    @endif
-                                                ">
-                                            до {{\Carbon\Carbon::parse($task->due_date)->format('d.m.Y')}}
-                                        </td>
-                                        <td class="text-secondary small mr-3">
-                                            {{$task->performer->name}}
-                                        </td>
-                                        <td>
+                        </td>
+                   </tr>
+                @empty
+                    <tr>
+                        <td colspan="5" class="text-center text-secondary font-italic">
+                            Нет задач для отображение
+                        </td>
+                    </tr>
+                @endforelse
+                </tbody>
 
-                                        </td>
-                                   </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="5" class="text-center text-secondary font-italic">
-                                            Нет задач для отображение
-                                        </td>
-                                    </tr>
-                                @endforelse
-                                </tbody>
-
-                            </table>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            </table>
         </div>
 
     </div>
+
 
 
 

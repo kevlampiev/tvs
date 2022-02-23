340f3b2d44c5cabb5beb1b6f84f522941597c84f
Константин Евлампиев <kevlamiev@gmail.com>
2022-02-23 13:43:01 +0700

    changed config/broadcasting.php set useTLS to false

--- resources/views/Admin/dashboard/toasts.blade.php
+++ resources/views/Admin/dashboard/toasts.blade.php
@@ -1,17 +1,4 @@
 
-{{--<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11" data-bs-autohide="false">--}}
-{{--    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">--}}
-{{--        <div class="toast-header">--}}
-{{--            <strong class="me-auto">Сообщение сервера</strong>--}}
-{{--            <small>11 мин назад</small>--}}
-{{--            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Закрыть"></button>--}}
-{{--        </div>--}}
-{{--        <div class="toast-body">--}}
-{{--            <a class="text-secondary" id="toast-data" href="#">Посмотреть</a>--}}
-{{--        </div>--}}
-
-{{--    </div>--}}
-{{--</div>--}}
 
 <script>
     Echo.private('user.{{auth()->user()->id}}')
@@ -35,9 +22,9 @@
                 callbacks: {
                     onTemplate: function() {
                            this.barDom.innerHTML =
-                               '<a class="text-white text-decoration-none" href="'+
+                               '<div class="p-2"><a class="text-white text-decoration-none" href="'+
                                 data.routeToRedirect+
-                                '">'+data.message+"</a>";
+                                '">'+data.message+"</a> </div>";
                         }
                     }
             }).show();

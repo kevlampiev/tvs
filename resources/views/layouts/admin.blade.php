01aef833da97157432d515f71e4c9d109a535279
Константин Евлампиев <kevlamiev@gmail.com>
2022-05-19 01:17:35 +0700

    fixed micro bug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,7 +39,7 @@
                         Техника
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href=href="{{route('admin.vehicles')}}">Список техники</a></li>
+                        <li><a class="dropdown-item" href="{{route('admin.vehicles')}}">Список техники</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item disabled" href="#">Отчет по состоянию техники</a></li>
                     </ul>
